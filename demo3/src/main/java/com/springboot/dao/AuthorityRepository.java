3729073144879154f6dc7670d0702388b61b9131
syedqadeer96 <99789405+syedqadeer96@users.noreply.github.com>
2022-05-17 18:25:08 +0530

    Delete demo3 directory

--- demo3/src/main/java/com/springboot/dao/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.dao;
-
-import com.springboot.entity.Authority;
-import com.springboot.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
--- demo3/src/main/java/com/springboot/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.service;
-import com.springboot.entity.Authority;
-import com.springboot.entity.User;
-
-public interface UserService {
-
-    public void save(User theUser, Authority authority);
-}
-
